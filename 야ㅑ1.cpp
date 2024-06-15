c10be1707b4ead13ad0683eae41df2feca1f2610
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-06-15 16:41:57 +0900

    Add files via upload

---
+++ 야ㅑ1.cpp
@@ -0,0 +1,63 @@
+//#include <stdio.h>
+//
+//int main( )
+//{
+//	printf("\'발명왕\'토마스 에디슨은 다음과 같이 말했다.\n");
+//	printf("\"천재는 1%%의 영감과 99%%의 노력으로 이루어진다.\"");
+//	return 0; 
+//} 
+//
+//#include<stdio.h>
+//
+//int main( )
+//{
+//	printf("저는%s.", "서천중학교\n");
+//	printf("%d 학년 %d반입니다 \n", 6, 3 );
+//	printf("몸무게와 키는 %f cm %lf kg\n", 157.56, 45.89);
+//	printf("나의 이름은%s 이고 혈액형은 %c 형입니다.\n", "홍길동", 'A'); 
+//	printf("제 오른쪽 시력은 %f 이고 제 왼쪽시력은 %f 입니다.", 1.0, 1.0);
+//	
+//	return 0;
+// }
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int grade = 6;
+//	int classs= 13;
+//	int number = 8;
+//	
+//	printf("%d학년 %d반 %d번", grade, classs, number);
+//	return 0;
+// } 
+//
+//#include<stdio.h>
+//int main( )
+//{
+//	printf("%d년 %d월 알파고 승리\n", 2019, 3);
+//	printf("%d%c 프린터로 집 짓기\n", 3, 'D');
+//	printf("원주율 = %f\n", 3.14) ;
+//	printf("%fKm 마라톤\n", 42.195);
+//	return 0; 
+//}
+//
+//#include<stdio.h>
+//int main( )
+//{
+//	printf("오늘 날짜는 %d년 %d월 %d일 입니다.\n",2024, 6, 15 );
+//	printf("현재 시간은 %d시 %d분 %d초 입니다.\n", 4, 15, 36);
+//	printf("밑변이 %d cm 높이가 %d cm인 삼각형의 넓이는 %d cm의 제곱입니다.\n",3, 4, 6);
+//	printf("원이 넓이는 %fcm 의 제곱  원의 둘레는 %f cm 입니다\n", 4.5*4.5*3.14, 9*3.14);
+//	printf("%d는 %f Km입니다.\n", 12378, 12.378);
+//	printf("%f kg은 %dg 입니다.",34.536,34536 );
+//	printf("34.5%%는 %d할 %d푼 %d리 입니다.", 3, 4, 5); 
+//	return 0;
+//	
+//}
+
+#include<stdio.h>
+int main( )
+{
+	printf("10진수  : %d, 8진수 : %o, 16진수 : %x", 255, 255, 255);
+	return 0;
+}
