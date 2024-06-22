61a5581674f8f536a21864e90883c651c712110a
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-06-22 16:45:42 +0900

    Add files via upload

---
+++ hello1.cpp
@@ -0,0 +1,97 @@
+//#include <stdio.h>
+//
+//int main( )
+//{
+//	int a;
+//	float f1;
+//	char ch;
+//	
+//	a = 15;
+//	f1 = 123.456;
+//	ch = 'A';
+//	printf("%d\n", a);
+//	printf("%f\n", f1);
+//	printf("%c = n", ch);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main ( )
+//{
+//	int num;
+//	printf("숫자를 하나 입력해주세요.");
+//	scanf("%d", &num);
+//	printf("입력하신 숫자는 %d입니다", num);
+//	
+//	return 0;	
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	float eye_l;
+//	float eye_r;
+//	printf("양쪽 시력을 입력하세요.:");
+//	scanf("%f %f", &eye_l, &eye_r);
+//	printf("왼쪽 시력: %.1f, 오른쪽 시력 :%.1f", eye_l, eye_r);
+//	return 0; 
+//}
+//
+//#include <stdio.h>
+//int main( )
+//{
+//	int grade;
+//	int classs;
+//	int number;
+//	
+//	
+//	scanf("%d %d %d", &grade, &classs, &number);
+//	printf("학년:%d 반:%d 이름:%d", grade, classs, number);
+//    return 0;	 
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	int a;
+//	int b;
+//	
+//	scanf("%d %d", &a, &b);
+//	printf("%d동 %d호", a, b);
+//	return 0; 
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	float length;
+//	
+//	scanf("%f", &length); // 32.54
+//	
+//	//3254cm, 325400mm
+//	printf("%fm는 %fcm이고, %fmm", length, length*100, length*10000);
+//	
+//	return 0;
+//}
+//#include <stdio.h>
+//int main( )
+//{
+//	int a;
+//	int b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a+b);
+//	printf("%d\n", a-b);
+//	printf("%d\n", a*b);
+//	printf("%d\n", a/b);
+//	return  0;
+//}
+
+#include <stdio.h>
+int main( )
+{
+	int a;
+	int b;
+	scanf("%d %d, &a");
+	printf("%d\n", a-b);
+	return 0;
+}
