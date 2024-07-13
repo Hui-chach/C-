b481b6931ffb8cf8929ab86c34d1a024cfdb62d0
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-07-13 16:47:08 +0900

    Add files via upload

---
+++ 오늘은 토요일 입니다.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	
+	a = 20;
+	b = 5;
+	
+	a += b;
+	printf("a = %d\n", a);
+	a *= b;
+	printf("a = %d\n", a);
+	a -= b;
+	printf("a = %d\n", a);
+	a %= b;
+	printf("a = %d\n", a);
+	return 0; 
+}
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	
+	a++;
+	a++;
+	printf("a = %d\n", a);
+	++a;
+	++a;
+	printf("a = %d\n", a);
+	return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	a = 10;
+	b = a++;
+	printf("a = %d")	 	  
+}
