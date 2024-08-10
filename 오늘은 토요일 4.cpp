5339d0b2bdee7e76564b13203a48e426c62712bc
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-08-10 16:47:55 +0900

    Add files via upload

--- 오늘은 토요일 4.cpp
+++ 오늘은 토요일 4.cpp
@@ -1,12 +1,49 @@
-#include <stdio.h>
-
-int main( )
+//#include<stdio.h>
+//int main( )
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for(int i = -n; i <= n; i++)
+//	{
+//		printf("%d", i);
+//	} 
+//	return 0;
+// } 
+////정수 n을 입력받고 -n까지 출력받는 프로그램
+//
+//#include <stdio.h>
+//int main( )
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for(int i =1;i<=n; i++)
+//	if (i%3 == 0) {
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//
+// } 
+//#include<stdio.h>
+//int main( )
+//{
+//	int a,b;
+//	int n;
+//	scanf("%d",&n);
+//	scanf("%d %d",&a ,&b);
+//	if(n%a == 0 && n%b == 0){
+//		printf("1");
+//	}
+//	else{
+//		printf("0");
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+int main()
 {
-	int a;
-	scanf("%d", &a);
-	if (a%7 == 0 && a%9 == 0){
-		printf("%d", a);
+	int n;
+	for (int i = 1; i<=1000; i++){
+		printf("%d\n", i);
 	}
-	
 	return 0;
 }
