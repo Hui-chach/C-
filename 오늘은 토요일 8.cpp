7c3015220014207093e22efbe76a9baf2f7e71c5
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-09-07 16:50:58 +0900

    Add files via upload

---
+++ 오늘은 토요일 8.cpp
@@ -0,0 +1,81 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double height[4];
+//	int j = 2;
+//	
+//	height[0] = 145.7;
+//	height[1] = 156.3;
+//	height[j] = 147.89;
+//	j++;
+//	height[j] = 159.4;
+//	printf()
+//	return 0;
+//	
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
+//	int br[10];
+//	
+//	for(int i = 0;i<=9;i++){
+//		printf("%d ", ar[i]);
+//	}
+//	
+//	
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
+//	int br[10];
+//	
+//	for(int i = 0;i<=9;i++){
+//		printf("%d ", ar[i]);
+//	}
+//	
+//	
+//	return 0;
+//}	
+
+
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	int ar[10] = {5,2,1,5,6,7,8,9,4,2}	
+	int min =  100
+	int index;
+	for(int i = 0; i<10; i++)
+	{
+		if(min>ar[i]){
+			min = ar[i]{}
+		}
+		{
+			
+			
+ } 
+ 
+ 
+ 
+ 
+ 
+ 
+ #include <stdio.h>
+ int main
+ {
+ 	
+ }
+
+
