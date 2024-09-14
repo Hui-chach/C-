c81bc9cf27836141535ee782991efa1b3cc42544
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-09-14 16:51:20 +0900

    Add files via upload

--- 오늘은 토요일 8.cpp
+++ 오늘은 토요일 8.cpp
@@ -1,33 +1,74 @@
-//#include <stdio.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
 //
 //int main()
 //{
-//	double height[4];
-//	int j = 2;
+//	srand(time(NULL));
+//	int arr[10];
+//	int a;
+//	int sum = 0;
+//	for(int i = 0; i<10;i++){
+//		arr[i] = rand()%9+1;
+//	}
 //	
-//	height[0] = 145.7;
-//	height[1] = 156.3;
-//	height[j] = 147.89;
-//	j++;
-//	height[j] = 159.4;
-//	printf()
-//	return 0;
+//	for(int i = 0; i<10;i++){
+//		sum+=arr[i];
+//		printf("%d", arr[i]);
+//	}
+//	printf(" %d", sum);
 //	
 //}
 
+
+//
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
-//	int br[10];
+//	char ch1, ch2;
+//	ch1 = 'A';
+//	ch2 = 'a'; 
 //	
-//	for(int i = 0;i<=9;i++){
-//		printf("%d ", ar[i]);
-//	}
+//	printf("%c", ch1+32);
+//	printf("%c", ch2-32);
 //	
 //	
+//	printf("%c %c\n", ch1, ch2);
+//	printf("%d %d\n", ch1, ch2);
+//	
+//	printf("%c %c\n", 66, 98);
+//	printf("%d %d\n", 66, 98);
 //	return 0;
+//}
+
+
+
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//	char ch;
+//	
+//	
+//	printf("문자를 입력하세요: ");
+//	ch = getchar();
+//	putchar(ch);
+//	printf("%c %d\n", ch, ch);
+//	return 0;
+//}
+
+
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//	char a[6] = "Hello";
+//	
+//	printf("%d", sizeof(a));
+//	printf("#d", a[5]);
 //}
 
 
@@ -35,47 +76,78 @@
 //
 //int main()
 //{
-//	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
-//	int br[10];
+//	int a,b;
+//	scanf("%d %d", &a,&b);
 //	
-//	for(int i = 0;i<=9;i++){
-//		printf("%d ", ar[i]);
-//	}
+//	char sss;
+//	char word[20];
 //	
-//	
+//	printf("단어를 입력하세요 : ");
+//	scanf("%s", word);
+//	printf("입력하신 단어는 %s 입니다", word);
 //	return 0;
-//}	
+//
+//}
 
-
-
-
-
-
+//#include <stdio.h>
+//int main()
+//{
+//	char sentence[80];
+//	
+//	puts("문장을 입력하세요 : ");
+//	gets(sentence);
+//	puts(sentence);
+//	printf("%s", sentence);
+//	return 0; 
+//}
+//
+//#include<stdio.h>
+//
+//int main(){
+//	char a;
+//	printf("입력 : "); 
+//	scanf("%c", &a);
+//	
+//	if(a>=65 && a<=91){
+//		printf("출력 : %c", a+32);
+//	
+//	}
+//	else if(a>=97 && a<=123)
+//	{
+//		printf("출력 : %c", a-32);
+//}
+//		
+//	}
+	
 #include <stdio.h>
-int main()
-{
-	int ar[10] = {5,2,1,5,6,7,8,9,4,2}	
-	int min =  100
-	int index;
-	for(int i = 0; i<10; i++)
-	{
-		if(min>ar[i]){
-			min = ar[i]{}
-		}
-		{
-			
-			
- } 
- 
- 
- 
- 
- 
- 
- #include <stdio.h>
- int main
- {
- 	
- }
-
-
+int main(){
+	for(int i = 65; i<=90; i++){
+		printf("%c", i);
+    }
+    for(int i = 97; i<=122; i++){
+    	printf("%c", i) ;
+    	
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    \
+    
+    
+    
+    
+#include <stdio.h>
+int main(){
+	
+	char word[20];
+	scanf("%s", word);
+	for(int i = 0;  word[i]; i++){
+		printf("%c", word[i]-32);
+	}
+	
+	scanf("%s", word);
+}
